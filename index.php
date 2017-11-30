d22b0df208686e55548722c105f5c131c053f324
Ashwin <ashwin.joshi12@live.com>
2017-11-30 10:28:03 -0600

    updated index.php:

--- index.php
+++ index.php
@@ -2,17 +2,43 @@
 <html>
 
 <head>
-    <title>Footer with social icons</title>
+    <title>EZRide</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="public/images/shortcut_icon.png">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/Footer-with-social-icons.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/index.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/carousel.css">
+    <script type="text/javascript" src="assets/js/carousel.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 </head>
 
 <body>
+<?php
+    session_start();
+?>
+<!-- Background carousel -->
+<div class="carousel slide carousel-fade" data-ride="carousel">
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+        </div>
+        <div class="item">
+        </div>
+        <div class="item">
+        </div>
+    </div>
+</div>
+<!-- Background carousel -->
+
+
 <div class="content">
     <!-- Navigation -->
     <nav class="navbar navbar-default">
@@ -24,13 +50,18 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a href="#" class="navbar-brand">Brand</a>
+        <a href="index.php" class="navbar-brand">
+            EZRide
+        </a>
     </div>
     <!-- Collection of nav links, forms, and other content for toggling -->
     <div id="navbarCollapse" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Profile</a></li>
+            <li class="active">
+                <a href="index.php">Home
+                    <span class="glyphicon glyphicon-home"></span>
+                </a>
+            </li>
         </ul>
         <form class="navbar-form navbar-left">
             <div class="input-group">
@@ -40,78 +71,125 @@
                 </span>
             </div>
         </form>
+
         <ul class="nav navbar-nav navbar-right">
             <li>
             <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                <?php
+                    if(isset($_SESSION["username"])){
+                        echo "Welcome, ".$_SESSION['fname'];
+                    }
+                ?>
                 <span class="glyphicon glyphicon-user"></span>
             </a>
-            <ul class="dropdown-menu">
-                <li><a href="assets/views/register.html">Sign-Up</li>
-                <li><a href="assets/views/login.html">Login</a></li>
-                <li><a href="">Logout</a></li>
+            <ul id="btn-list" class="dropdown-menu">
+                <li>
+                    <?php if(!isset($_SESSION["username"])){ ?>
+                        <a id="sign-up" href="assets/views/register.html">
+                        Sign-Up
+                        </a>
+                    <?php }?>
+                </li>
+                <li>
+                    <?php if(!isset($_SESSION["username"])){ ?>
+                        <a id="log-in" href="assets/views/login.html">
+                        Login
+                        <span class="glyphicon glyphicon-log-in"></span>
+                        </a>
+                    <?php }?>
+                </li>
+                <li>
+                    <?php if(isset($_SESSION["username"])){ ?>
+                        <a id="order-list" href="#">My Orders
+                        </a>
+                    <?php }?>
+                </li>
+                <li>
+                    <?php if(isset($_SESSION["username"])){ ?>
+                        <a id="log-out" href="assets/php/logout.php">Logout
+                        <span class="glyphicon glyphicon-log-out"></span>
+                        </a>
+                    <?php }?>
+                </li>
             </ul>
             </li>
         </ul>
+        <!-- Order cart controller -->
+        <?php if(isset($_SESSION["username"])){ ?>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="">
+                    My Cart
+                    <span class="glyphicon glyphicon glyphicon-shopping-cart"></span>
+                    </a>
+                </li>
+            </ul>
+        <?php }?>
+        <!-- Order cart controller -->
+
+        <!-- admin controller -->
+        <?php if(isset($_SESSION["admin"])){ if($_SESSION["admin"] == 1){ ?>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="">
+                    Admin
+                    <span class="glyphicon glyphicon glyphicon-cog"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="assets/views/carview.php">
+                                Car List
+                            </a>
+                        </li>
+                        <li>
+                            <a href="assets/views/addcarform.php">
+                                Add new cars
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        <?php }}?>
+        <!-- admin controller -->
+
+
     </div>
     </nav>
 
     <!-- Page Content -->
     <div class="container">
-        <form id="car-rental-form" class="form-horizontal">
-  <div class="form-group">
-    <label for="pick-up-location" class="control-label col-sm-2">Pick-Up Location:</label>
-    <div class="col-sm-2">
-      <select id="pick-up-location" class="form-control empty">
-        <option selected="selected" disabled="disabled" style="display:none;">Select Location</option>
-        <option>Dallas</option>
-        <option>Irving</option>
-        <option>Austin</option>
-        <option>Houston</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="pick-up-date" class="control-label col-sm-2">Pick-Up Date:</label>
-      <div class="col-sm-2">
-        <input id="pick-up-date" type="date" class="form-control"/>
-      </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="drop-off-location" class="control-label col-sm-2">Drop-Off Location:</label>
-    <div class="col-sm-2">
-      <select id="drop-off-location" class="form-control">
-        <option selected="selected" disabled="disabled" style="display:none;">Select Location</option>
-        <option>Dallas</option>
-        <option>Irving</option>
-        <option>Austin</option>
-        <option>Houston</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="drop-off-date" class="control-label col-sm-2">Drop-Off Date:</label>
-      <div class="col-sm-2">
-        <input id="drop-off-date" type="date" class="form-control"/>
-      </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="car-type" class="control-label col-sm-2">Car Type:</label>
-    <div class="col-sm-2">
-      <select id="car-type" class="form-control">
-        <option selected="selected" disabled="disabled" style="display:none;">Select Car Type</option>
-        <option>Hatchback</option>
-        <option>Sedan</option>
-        <option>SUV</option>
-        <option>Semi</option>
-      </select>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default">Submit</button>
-    </div>
-  </div>
-</form>
+        <form id="car-rental-form" class="form-inline" method="POST" action="assets/php/viewcars.php" enctype="multipart/form-data">
+            <h3>Rent a car</h3>
+            <hr noshade>
+            <div class="form-group">
+                <select name="pick-up-location" id="pick-up-location" class="form-control empty" required>
+                    <option selected="selected" disabled="disabled" style="display:none;">Pick up location</option>
+                    <option value="Dallas">Dallas</option>
+                    <option value="Irving">Irving</option>
+                    <option value="Austin">Austin</option>
+                    <option value="Houston">Houston</option>
+                </select>
+                <input name="pick-up-date" id="pick-up-date" type="text" class="form-control" placeholder="Pick up date" / required>
+                <select name="drop-off-location" id="drop-off-location" class="form-control" required>
+                    <option selected="selected" disabled="disabled" style="display:none;">Drop-off location</option>
+                    <option value="Dallas">Dallas</option>
+                    <option value="Irving">Irving</option>
+                    <option value="Austin">Austin</option>
+                    <option value="Houston">Houston</option>
+                </select>
+                <input name="drop-off-date" id="drop-off-date" type="text" class="form-control" placeholder="Drop-off date" required />
+                <select name="car-type" id="car-type" class="form-control" required>
+                    <option selected="selected" disabled="disabled" style="display:none;">Select Car Type</option>
+                    <option value="hatchback">Hatchback</option>
+                    <option value="sedan">Sedan</option>
+                    <option value="suv">SUV</option>
+                  </select>
+            </div>
+            <div class="form-group">
+                <input id="car-search" type="submit" name="search" value="Search" class="btn btn-success">
+                <input id="reset" type="reset" name="reset" value="Reset" class="btn btn-danger">
+            </div>
+        </form>
     </div>
 
 
