41ce98cf19dd6194b882acb85bcb445c5a217182
Ashwin <ashwin.joshi12@live.com>
2017-12-03 21:43:28 -0600

    fixed redirection of user to cart/orders view

--- assets/php/deleteorder.php
+++ assets/php/deleteorder.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	if(isset($_POST["delete-order"])){
+	if(isset($_POST["delete-order-from-orders"])){
 		$order_id = $_POST["order-id"];
 		$car_id = $_POST["car-id"];
 		include 'dbconnect.php';
@@ -9,9 +9,29 @@
 		$sql2 = "UPDATE car SET status=false WHERE _id = '$car_id'";
 		$res2 = mysqli_query($conn, $sql2);
 		if($res1 && $res2){
+			
 			header("Location: ../views/orders.php");
 		}else{
+			
 			header("Location: ../views/orders.php");
+		}
+	}
+
+	if(isset($_POST["delete-order-from-cart"])){
+		$order_id = $_POST["order-id"];
+		$car_id = $_POST["car-id"];
+		$user_id = $_SESSION["uid"];
+		include 'dbconnect.php';
+		$sql1 = "DELETE FROM rent_order WHERE order_id='$order_id'";
+		$res1 = mysqli_query($conn, $sql1);
+		$sql2 = "UPDATE car SET status=false WHERE _id = '$car_id'";
+		$res2 = mysqli_query($conn, $sql2);
+		if($res1 && $res2){
+			
+			header("Location: ../views/cartview.php?id=$user_id");
+		}else{
+			
+			header("Location: ../views/cartview.php?id=$user_id");
 		}
 	}
 ?>
